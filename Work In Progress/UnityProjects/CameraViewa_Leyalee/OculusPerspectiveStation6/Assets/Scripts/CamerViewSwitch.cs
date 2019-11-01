922d5a966faed0c418790c7b79a5c85327f64d34
Leyya Alsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-11-01 16:49:52 -0400

    added unity project for the two camera views

---
+++ Work In Progress/UnityProjects/CameraViewa_Leyalee/OculusPerspectiveStation6/Assets/Scripts/CamerViewSwitch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerViewSwitch : MonoBehaviour
+{
+
+    public Camera Minerva_Camera;
+    public Camera S5ANT_Camera;
+
+    int cameraState;
+    float waitTime;
+    float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraState = 1;
+        waitTime = 2.0f;
+        currentTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cameraState *= -1;
+            currentTime -= waitTime;
+        }
+
+        if(cameraState == 1)
+        {
+            minervaCameraView();
+        }
+
+        if(cameraState == -1)
+        {
+            botView();
+        }
+
+    }
+
+    private void botView()
+    {
+        Minerva_Camera.enabled = false;
+        S5ANT_Camera.enabled = true;
+    }
+
+    private void minervaCameraView()
+    {
+        Minerva_Camera.enabled = true;
+        S5ANT_Camera.enabled = false;
+    }
+}
---
+++ Work In Progress/UnityProjects/CameraViewa_Leyalee/OculusPerspectiveStation6/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    float inputX;
+    float inputZ;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        inputX = Input.GetAxis("Horizontal") * speed;
+        inputZ = Input.GetAxis("Vertical") * speed;
+
+        if(inputX != 0)
+        {
+            rotateCamera();
+        }
+
+        if(inputZ != 0)
+        {
+            moveCamera();
+        }
+
+    }
+
+    private void moveCamera()
+    {
+        transform.position += transform.forward * inputZ * Time.deltaTime;
+    }
+
+    private void rotateCamera()
+    {
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime, 0f));
+
+    }
+}
