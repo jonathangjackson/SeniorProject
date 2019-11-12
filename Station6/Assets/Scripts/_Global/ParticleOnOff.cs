b446b990730f4bdea2bcd6322407b1a13dea5d11
CUNET\Jonathangjackson <jonathangjackson@cmail.carleton.ca>
2019-11-12 09:17:09 -0500

    Added Thermal Vision Scripts to Master

---
+++ Station6/Assets/Scripts/_Global/ParticleOnOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOnOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            this.GetComponent<ParticleSystem>().Clear();
+            this.GetComponent<ParticleSystem>().Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
