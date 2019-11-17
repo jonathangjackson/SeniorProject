8b469eb9c28c1ba0603603486869b5c5525d2153
CUNET\leyaleealsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-11-17 13:40:45 -0500

    added the menu UI toggle

---
+++ Station6/Assets/Scripts/_Global/ToggleArmMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class ToggleArmMenu : MonoBehaviour
+{
+
+    public GameObject armMenu;
+    bool menuOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (OVRInput.GetDown(OVRInput.Button.Three)){
+            menuOn = !menuOn;
+
+            armMenu.SetActive(menuOn);
+        }
+
+    }
+}
