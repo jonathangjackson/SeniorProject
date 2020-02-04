a9d678669177b45c0d95e59086271f9c706c7bee
YuFeng Yao <YuFengYao@cunet.carleton.ca>
2020-02-03 20:28:12 -0500

    teleportation COMPLETE

---
+++ Station6/Assets/Scripts/Portal/Hacking Puzzle/TeleportPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            thePlayer.transform.position = teleportTarget.transform.position;
+            Debug.Log("test");
+        }
+    }
+}
