0a51541e21876f67b85a8c6e9f6f4c1d7792f098
YuFeng Yao <YuFengYao@cunet.carleton.ca>
2020-02-03 20:24:13 -0500

    jank shit

---
+++ SeniorProject/Assets/Scripts/Portal Scripts/Hacking Puzzle/TeleportPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    private IEnumerator coroutine;
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    public GameObject VRMovement;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("test");
+        if (other.tag == "Player")
+        {
+            VRMovement.GetComponent<VRMovementOculus>().minerSwitchOn = true;
+            thePlayer.transform.position = teleportTarget.transform.position;
+            Debug.Log("test");
+            coroutine = WaitAndPrint(2.0f);
+            StartCoroutine(coroutine);
+        }
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        VRMovement.GetComponent<VRMovementOculus>().minerSwitchOn = false;
+
+    }
+}
