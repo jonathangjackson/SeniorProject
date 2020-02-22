14c9117a11e453e2a15b762efbe48a4acef9617a
Nick Chojnacki <nickchojnacki24@gmail.com>
2020-02-22 18:56:14 -0500

    Icon system Implemented

---
+++ SeniorProject/Assets/Scripts/VRwaypoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VRwaypoint : MonoBehaviour
+{
+    public Camera VRcamera;
+    public Image image;
+    public Transform target;
+    public Text meter;
+    public Canvas canvas;
+
+    public Vector3 ScaleFactor;
+    public Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        meter.text = ((int)Vector3.Distance(target.position, VRcamera.transform.position)).ToString() + "m";
+        canvas.transform.LookAt(VRcamera.transform.position + VRcamera.transform.rotation * Vector3.forward, VRcamera.transform.rotation * Vector3.up);
+        canvas.transform.Rotate(0, 180, 0);
+
+        canvas.transform.localScale = Vector3.Distance(VRcamera.transform.position, canvas.transform.position) * ScaleFactor;
+
+        canvas.transform.position = (target.position + offset);
+
+    }
+}
---
+++ SeniorProject/Assets/Scripts/rotateIcon.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class rotateIcon : MonoBehaviour
+{
+
+    public Image icon;
+
+    float speed = 1.0f;
+
+    public Transform canvas;
+
+    Animator animator;
+
+    public Image informationBackground;
+
+    public Text infoText;
+
+    public Text meters;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = canvas.GetComponent<Animator>();
+
+        informationBackground.enabled = false;
+        infoText.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void swapIcon()
+    {
+       print("Change Icons");
+
+        icon.enabled = false;
+        meters.enabled = false;
+
+
+
+        informationBackground.enabled = true;
+        infoText.enabled = true;
+    }
+
+    void walkOut()
+    {
+        print("Change Back");
+
+        icon.enabled = true;
+        meters.enabled = true;
+
+
+
+        informationBackground.enabled = false;
+        infoText.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "MainCamera")
+        {
+            print("Collision, can spin icon");
+            animator.SetTrigger("Spin");
+            animator.ResetTrigger("Reverse");
+
+        }
+        else
+        {
+            print("No collision, cant spin icon");
+        }
+
+}
+
+    void OnTriggerStay(Collider other)
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+
+        animator.ResetTrigger("Spin");
+
+        animator.SetTrigger("Reverse");
+    }
+}
