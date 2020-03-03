3f71216de470b1dd9720ee3fa47267356bffe0d6
CUNET\Jonathangjackson <jonathangjackson@cmail.carleton.ca>
2020-03-03 12:40:56 -0500

    Tutorial steps 1-3 implemented

---
+++ SeniorProject/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public TutorialManager tm;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "tutorial")
+        {
+            tm.triggerNext();
+            Destroy(this);
+        }
+    }
+}
---
+++ SeniorProject/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public List<AudioClip> audioClips;
+    public List<GameObject> tutorialObjects;
+    public AudioSource audio;
+    private bool clipPlay = false;
+    
+    void Start()
+    {
+        playAudioClip(audioClips[0]);
+        clipPlay = true;
+    }
+
+    void Update()
+    {
+        if (!audio.isPlaying && clipPlay)
+        {
+            onClipEnd();
+        }
+    }
+
+    void playAudioClip(AudioClip ac)
+    {
+        audio.Stop();
+        audio.clip = ac;
+        audio.Play();
+        clipPlay = true;
+    }
+
+    public void triggerNext()
+    {
+        if (audioClips.Count > 0)
+        {
+            tutorialObjects[0].SetActive(false);
+            tutorialObjects.RemoveAt(0);
+            audioClips.RemoveAt(0);
+            playAudioClip(audioClips[0]);
+            
+        }
+        else
+        {
+            Debug.Log("Tutorial Done");
+        }
+    }
+
+    public void onClipEnd()
+    {
+        tutorialObjects[0].SetActive(true);
+        clipPlay = false;
+    }
+}
