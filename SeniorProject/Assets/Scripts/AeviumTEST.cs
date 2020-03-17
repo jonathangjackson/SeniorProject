7bd22c81113cc2e4610ebecae359d418ed34f2d9
YuFeng <yufengyao913@gmail.com>
2020-03-17 12:09:32 -0400

    aevium particle effects take 2

---
+++ SeniorProject/Assets/Scripts/AeviumTEST.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AeviumTEST : MonoBehaviour
+{
+    public GameObject AeviumParticleSystem, AeviumDrip, AeviumDrip2, AeviumCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AeviumParticleSystem.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            AeviumParticleSystem.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AeviumDrip.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            AeviumDrip.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            AeviumDrip2.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            AeviumDrip2.SetActive(false);
+        }
+
+    }
+}
