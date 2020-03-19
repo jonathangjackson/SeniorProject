257e1c1c303cc90b5785bb692e053885c6a677ce
Nick Chojnacki <nickchojnacki24@gmail.com>
2020-03-19 18:28:14 -0400

    Added new lever puzzle that works with proximity, we still need lever interaction to be combined with it

---
+++ SeniorProject/Assets/Scripts/Lab1Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab1Door : MonoBehaviour
+{
+    Animator animator1;
+    AudioSource audio1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator1 = GetComponent<Animator>();
+        audio1 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            audio1.PlayOneShot(audio1.clip);
+
+            animator1.Play("Door_Open");
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        animator1.Play("Door_Close");
+        audio1.Play();
+    }
+}
---
+++ SeniorProject/Assets/Scripts/Lab2Doors.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab2Doors : MonoBehaviour
+{
+
+    Animator animator2;
+    AudioSource audio2;
+
+
+    public Lever2 lever2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator2 = GetComponent<Animator>();
+        audio2 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (lever2.lever2active == true)
+        {
+            if (collider.gameObject.name == "Player")
+            {
+                audio2.PlayOneShot(audio2.clip);
+
+                animator2.Play("Door_Open");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        animator2.Play("Door_Close");
+        audio2.Play();
+    }
+}
