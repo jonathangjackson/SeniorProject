4f511ebedc27ca3592ecd902a886d7065f2dfdf6
jonathanghackson <jonathangjackson@cmail.carleton.ca>
2020-03-19 11:23:02 -0400

    Pod Anim Controller, Air Lock Anim Controller added

---
+++ SeniorProject/Assets/Scripts/ControlAnimations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimations : MonoBehaviour
+{
+    public Animator animatorController;
+    public bool triggerCollision = false;
+    public bool externalCollision = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (animatorController == null && triggerCollision)
+            animatorController = this.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && triggerCollision)
+        {
+            animatorController.SetBool("Play", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && triggerCollision)
+        {
+            animatorController.SetBool("Play", false);
+        }
+    }
+
+    private void Update()
+    {
+        if (externalCollision)
+        {
+            animatorController.SetBool("Play", false);
+        }
+    }
+}
