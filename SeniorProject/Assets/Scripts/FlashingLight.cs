191488a2ffbd0972c17df0663917acd49a0dbebc
jonathanghackson <jonathangjackson@cmail.carleton.ca>
2020-03-23 07:03:28 -0400

    Added Flashing Lights Script

---
+++ SeniorProject/Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    public List<GameObject> lightObjects = new List<GameObject>();
+    public Material emissionMat;
+    public bool setFlashing = false;
+    private float timer = 0.0f;
+
+    private bool flasing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (setFlashing)
+        {
+            setFlashing = false;
+            flasing = true;
+            for(int i = 0; i < lightObjects.Count; i++)
+            {
+                lightObjects[i].GetComponent<Renderer>().material = emissionMat;
+            }
+        }
+        if (flasing)
+        {
+            if (timer < 1.0f)
+            {
+
+            }
+        }
+    }
+}
