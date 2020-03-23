5e06fc2b55e2566554bf1153db696dd34506da7d
YuFeng <yufengyao913@gmail.com>
2020-03-23 07:36:42 -0400

    lab 2 and lab 3 done

--- SeniorProject/Assets/Scripts/AeviumLab3.cs
+++ SeniorProject/Assets/Scripts/AeviumLab3.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AveiumInteraction : MonoBehaviour
+public class AeviumLab3 : MonoBehaviour
 {
-    public GameObject AeviumParticleSystem, AeviumDrip, AeviumDrip2, AeviumCube, Tube;
+    public GameObject AeviumParticleSystem, AeviumDrip, AeviumCube, Tube;
 
     public Material CubeDissolve, Tube01, Tube02, Tube03;
 
-    public float time = 7.5f;
+    public float time = 5f;
 
     private IEnumerator coroutine;
 
     public bool ButtonPressed = false;
-    public bool Lab02Done = false;
+    public bool Lab03Done = false;
 
     public int ButtonPressCounter = 0;
 
@@ -24,42 +24,36 @@ public class AveiumInteraction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {    
-        if (Input.GetKeyDown(KeyCode.B) && ButtonPressCounter < 3)
+    {     
+        if (Input.GetKeyDown(KeyCode.N) && ButtonPressCounter < 3)
 
-        {
-            ButtonPressed = true;
-            ButtonPressCounter += 1;
-        }
+            {
+                ButtonPressed = true;
+                ButtonPressCounter += 1;
+            }
 
         if (ButtonPressed == true)
         {
             AeviumParticleSystem.SetActive(true);
             button.GetComponent<CapsuleCollider>().enabled = false;
 
-            coroutine = WaitAndPrint(7.5f);
+            coroutine = WaitAndPrint(5.0f);
             StartCoroutine(coroutine);
             time -= Time.deltaTime;
-            if (time < 7)
+            if (time < 4)
             {
                 AeviumDrip.SetActive(true);
-            }
-
-            if (time < 5.5)
-            {
-                AeviumDrip2.SetActive(true);
             }
 
             if (time < 0 && ButtonPressCounter < 3)
             {
                 AeviumParticleSystem.SetActive(false);
-                AeviumDrip.SetActive(false);
-                AeviumDrip2.SetActive(false);
+                AeviumDrip.SetActive(false);                
                 //ButtonPressCounter += 1;
                 ButtonPressed = false;
                 button.GetComponent<CapsuleCollider>().enabled = true; ;
@@ -70,28 +64,27 @@ public class AveiumInteraction : MonoBehaviour
             {
                 AeviumParticleSystem.SetActive(false);
                 AeviumDrip.SetActive(false);
-                AeviumDrip2.SetActive(false);
                 //ButtonPressCounter += 1;
                 ButtonPressed = false;
                 button.GetComponent<CapsuleCollider>().enabled = true; ;
             }
 
-                if (ButtonPressCounter == 1 && time < 2.32)
+            if (ButtonPressCounter == 1 && time < 1.8)
             {
                 Tube.GetComponent<Renderer>().material = Tube01;
             }
 
-            if (ButtonPressCounter == 2 && time < 2.32)
+            if (ButtonPressCounter == 2 && time < 1.8)
             {
                 Tube.GetComponent<Renderer>().material = Tube02;
             }
 
-            if (ButtonPressCounter == 3 && time < 2.32)
+            if (ButtonPressCounter == 3 && time < 1.8)
             {
                 Tube.GetComponent<Renderer>().material = Tube03;
                 //AeviumCube.GetComponent<Renderer>().material = CubeDissolve;
-                Lab02Done = true;
-            }          
+                Lab03Done = true;
+            }
 
         }
 
