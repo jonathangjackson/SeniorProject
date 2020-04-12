52ba3a4eaef191a49fc3982f4e8398ffe5c01fe9
YuFeng <yufengyao913@gmail.com>
2020-04-12 04:12:39 -0400

    added walking sound, basic end credits

---
+++ SeniorProject/Assets/DelayAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour
+{
+    public Animator animtest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animtest.gameObject.GetComponent<Animator>().enabled = false;
+        StartCoroutine(PlayEndCredits());
+        //animtest = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PlayEndCredits()
+    {
+        yield return new WaitForSeconds(5);
+        animtest.gameObject.GetComponent<Animator>().enabled = true;
+    }
+}
